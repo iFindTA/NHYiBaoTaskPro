0058f30410b720283e0d2ca5315ea92360823743
hu jiaju <nanhujiaju@163.com>
2016-08-16 10:54:15 +0800

    Initial Commit

---
+++ NHYiBaoTaskPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NHYiBaoTaskPro
+//
+//  Created by hu jiaju on 16/8/16.
+//  Copyright © 2016年 Nanhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NHYiBaoTaskPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NHYiBaoTaskPro
+//
+//  Created by hu jiaju on 16/8/16.
+//  Copyright © 2016年 Nanhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
